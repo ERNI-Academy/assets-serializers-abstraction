8aedb782389aee791b2ab012f94ae6bfcb90854a
Omar Amalfi Camacho <omar.amalfi@ypsomed.com>
2022-04-07 15:47:57 +0200

    add unit tests projects

---
+++ tests/EA.Serializers.UnitTests/SerializersTests/MessagePackTests.cs
@@ -0,0 +1,10 @@
+﻿using EA.Serializers.Contracts;
+
+namespace EA.Serializers.UnitTests.SerializersTests;
+
+public class MessagePackTests : BaseTests
+{
+    protected override string ContentType => "application/x-net-messagepack";
+
+    protected override ISerializer BuildSut() => new EA.Serializers.MessagePack.MessagePackSerializer();
+}
---
+++ tests/EA.Serializers.UnitTests/SerializersTests/NewtonsoftJsonTests.cs
@@ -0,0 +1,10 @@
+﻿using EA.Serializers.Contracts;
+
+namespace EA.Serializers.UnitTests.SerializersTests;
+
+public class NewtonsoftJsonTests : BaseTests
+{
+    protected override string ContentType => "application/json";
+
+    protected override ISerializer BuildSut() => new EA.Serializers.NewtonsoftJson.JsonSerializer();
+}
